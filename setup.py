f9298cac6efdb0f5ff81a802e7dcb686e9627950
Jeffrey Wilson <m00dimus@gmail.com>
2016-02-09 19:45:54 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name			= 'resource_217',
+	version			= '1.0.0',
+	py_modules		= '[resource_217'],
+	author			= 'm00dimus',
+	author_email	= 'm00dimus@gmail.com',
+	url				= 'https://github.com/m00dimus/resource_217',
+	description		= 'Common Python resources not always included as built in functions (BIF)'
+	)
