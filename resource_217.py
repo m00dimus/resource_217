d7ff753d1f69ee4499de43a940f94820dac7644f
Jeffrey Wilson <m00dimus@gmail.com>
2016-02-13 00:01:00 -0600

    packaging

---
+++ resource_217.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""Convenience wrapper for running directly from source tree."""
+
+
+from resource_217.resource_217 import main
+
+
+if __name__ == '__main__':
+    main()
---
+++ resource_217/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+
+"""resource_217.__main__: executed when bootstrap directory is called as script."""
+
+
+from .resource_217 import main
+main()
