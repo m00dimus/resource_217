396893cd84cdf1a1009b8939cdc71ef3d21eeaae
Jeffrey Wilson <m00dimus@gmail.com>
2016-12-26 01:58:20 -0600

    Added get_formated_timestamp, isvaliddate, gettimestampoffset functions for resource_datetime.py

--- resource_217/resource_datetime.py
+++ resource_217/resource_datetime.py
@@ -26,8 +26,29 @@ def get_current_time():
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime(dformat)
     return st
-    
+
 def get_current_formal_date():
 	ts = time.time()
 	st = datetime.datetime.fromtimestamp(ts).strftime('%A, %B %d, %Y')
 	return st
+
+def get_formated_timestamp(dvalue,sDateFormat='%Y-%m-%d %H:%M:%S'):
+    result = datetime.datetime.fromtimestamp(dvalue).strftime(sDateFormat)
+    return result
+    
+def isvaliddate(sValue,sDateFormat='%Y-%m-%d %H:%M:%S'):
+    try:
+        datetime.datetime.strptime(sValue, sDateFormat)
+    except ValueError:
+        #raise ValueError("Incorrect data format, should be YYYY-MM-DD HH:mm:ss")
+        return False
+    return True
+
+def gettimestampoffset(sValue,sDateFormat='%Y-%m-%d %H:%M:%S',d=0,s=0):
+    if isvaliddate(sValue,sDateFormat):
+        sCurrentValue = datetime.datetime.strptime(sValue, sDateFormat)
+        sOffestValue = sCurrentValue + datetime.timedelta(days=d,seconds=s)
+        result = sOffestValue.strftime(sDateFormat)
+        return result
+    else:
+        return False
