8b70b193d59d34eefd48dffead97e536ccc3c62f
Jeffrey Wilson <m00dimus@gmail.com>
2019-09-28 17:01:38 -0500

    Added leading zeros and string spacing functions and test cases

--- resource_217/resource_string.py
+++ resource_217/resource_string.py
@@ -5,8 +5,9 @@
 #// Email: m00dimus@gmail.com
 #// Version: 0.2-20160114
 #// ------------------------------------
+import re
 
-def isconsonant(s):
+def isconsonant(s=''):
     """ Check if character is a consonant """
     #// accept only strings with length of 1
     if not isinstance(s,str) or len(s) > 1:
@@ -16,7 +17,7 @@ def isconsonant(s):
         return True
     return False
 
-def isvowel(s):
+def isvowel(s=''):
     """ Check if character is a vowel """
     #// accept only strings with length of 1
     if not isinstance(s,str) or len(s) > 1:
@@ -25,3 +26,38 @@ def isvowel(s):
     if s.isalpha() and s.lower() in ['a','e','i','o','u']:
         return True
     return False
+
+def stringspacing(sValue='',doublespace=False):
+    """ Enforce standard spacing through a string. """
+    result=sValue
+
+    #// force polyspace to single space
+    while "  " in result:
+        result=result.replace("  "," ")
+
+    #// remove leading space
+    if result.startswith(" "):
+        result=result[1:]
+
+    #// override to doublespace
+    if doublespace:
+        result=result.replace(" ","  ")
+
+    return result
+
+def suppressleadingzeros(sValue=''):
+    """ Suppress leading zeros in string """
+    result=sValue
+
+    searchstr=re.compile("(\w(\s)?)+")
+    #// evalulate word in string
+    if re.match(searchstr,result):
+        resultlist=sValue.split(' ')
+        result=[]
+        for item in resultlist:
+            while item[:1] == "0":
+                item=item[1:]
+            result.append(item)
+        result=" ".join(result)
+
+    return result
--- resource_217/test_string.py
+++ resource_217/test_string.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 #// ------------------------------------
 #// Title: test_string.py
 #// Description: Test examples for
@@ -6,80 +7,241 @@
 #// Email: m00dimus@gmail.com
 #// Version: 0.2-20160115 (python 3.4.3)
 #// ------------------------------------
-
+import unittest
 import resource_string as test
 
-def main(): 
-    '''
-    Test string module
-    '''
-    print('Test examples for resource_string.py')
+class TestString(unittest.TestCase):
+    def test_001_isvowel(self):
+        """ Test if vowel is a vowel """
 
-    s_lower=['a','b','c','d','e','f','g','h',
-             'i','j','k','l','m','n','o','p',
-             'q','r','s','t','u','v','w','x','y','z']
-    print('Test all lower consonants')
-    for s in s_lower:
-        if test.isconsonant(s):
-            print(s + ' is a consonant')
-        else:
-            pass
-            #print(s + ' is not a consonant')
-            
-    s_upper=['A','B','C','D','E','F','G','H',
-             'I','J','K','L','M','N','O','P',
-             'Q','R','S','T','U','V','W','X','Y','Z']
+        testlist=['a','e','i','o','u']
+        for val in testlist:
+            got=test.isvowel(val)
+            self.assertTrue(got)
 
-    print('\n') 
-    print('Test all upper consonants')
-    for s in s_upper:
-        if test.isconsonant(s):
-            print(s + ' is a consonant')
-        else:
-            pass
-            #print(s + ' is not a consonant')
+    def test_001_isvowel_upper(self):
+        """ Test if vowel is a vowel """
 
-    print('\n') 
-    s_lower=['a','b','c','d','e','f','g','h',
-             'i','j','k','l','m','n','o','p',
-             'q','r','s','t','u','v','w','x','y','z']
-    print('Test all lower vowels')
-    for s in s_lower:
-        if test.isvowel(s):
-            print(s + ' is a vowel')
-        else:
-            pass
-            #print(s + ' is not a vowel')
-            
-    s_upper=['A','B','C','D','E','F','G','H',
-             'I','J','K','L','M','N','O','P',
-             'Q','R','S','T','U','V','W','X','Y','Z']
+        testlist=['a','e','i','o','u']
+        for val in testlist:
+            got=test.isvowel(val.upper())
+            self.assertTrue(got)
 
-    print('\n') 
-    print('Test all upper vowels')
-    for s in s_upper:
-        if test.isvowel(s):
-            print(s + ' is a vowel')
-        else:
-            pass
-            #print(s + ' is not a vowel')
+    def test_001_isnotvowel(self):
+        """ Test if not a vowel is a vowel """
 
-    s_testset=['1',1,3.14,'!','abc',(1,2,'a')]
+        testlist=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+        for val in testlist:
+            got=test.isvowel(val)
+            self.assertFalse(got)
 
-    print('\n') 
-    print('Test non alphabet characters')
-    for s in s_testset:
-        if test.isconsonant(s):
-            print(str(s) + ' is a consonant')
-        else:
-            print(str(s) + ' is not a consonant')
+    def test_001_isnotvowel_upper(self):
+        """ Test if not a vowel is a vowel """
 
-        if test.isvowel(s):
-            print(str(s) + ' is a vowel')
-        else:
-            print(str(s) + ' is not a vowel')
+        testlist=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+        for val in testlist:
+            got=test.isvowel(val.upper())
+            self.assertFalse(got)
 
+    def test_001_invalid_vowel(self):
+        """ Test for invalid inputs """
+        testlist=['1',1,3.14,'!','abc',(1,2,'a')]
+        for val in testlist:
+            got=test.isvowel(val)
+            self.assertFalse(got)
 
+    def test_001_isconsonant(self):
+        """ Test if consonant is a consonant """
+
+        testlist=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+        for val in testlist:
+            got=test.isconsonant(val)
+            self.assertTrue(got)
+
+    def test_001_isconsonant_upper(self):
+        """ Test if consonant is a consonant """
+
+        testlist=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+        for val in testlist:
+            got=test.isconsonant(val.upper())
+            self.assertTrue(got)
+
+    def test_001_isnotconsonant(self):
+        """ Test if not a consonant is a consonant """
+
+        testlist=['a','e','i','o','u']
+        for val in testlist:
+            got=test.isconsonant(val)
+            self.assertFalse(got)
+
+    def test_001_isnotconsonant_upper(self):
+        """ Test if not a consonant is a consonant """
+
+        testlist=['a','e','i','o','u']
+        for val in testlist:
+            got=test.isconsonant(val.upper())
+            self.assertFalse(got)
+
+    def test_001_invalid_consonant(self):
+        """ Test for invalid inputs """
+        testlist=['1',1,3.14,'!','abc',(1,2,'a')]
+        for val in testlist:
+            got=test.isconsonant(val)
+            self.assertFalse(got)
+
+    def test_001_suppressleadingzeros(self):
+        """ Test to suppress leading zeros """
+
+        val='01'
+        want='1'
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+    def test_001_suppressleadingzeros_blank(self):
+        """ Test to suppress leading zeros for blank input"""
+
+        val=''
+        want=''
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+    def test_001_suppressleadingzeros_allzeros(self):
+        """ Test to suppress leading zeros for all zeros """
+
+        val='0000'
+        want=''
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+    def test_001_suppressleadingzeros_return(self):
+        """ Test to suppress leading zeros for no changes"""
+
+        val='1234'
+        want='1234'
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+    def test_001_suppressleadingzeros_float(self):
+        """ Test to suppress leading zeros in float value as string"""
+
+        val='03.145'
+        want='3.145'
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+    def test_001_suppressleadingzeros_more(self):
+        """ Test to suppress one or more leading zeros """
+
+        val='0001'
+        want='1'
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+        val='000000000001'
+        want='1'
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+    def test_001_suppressleadingzeros_string(self):
+        """ Test to suppress leading zeros """
+
+        val='one 01 two'
+        want='one 1 two'
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+    def test_001_suppressleadingzeros_more(self):
+        """ Test to suppress one or more leading zeros """
+
+        val='one 0001 two'
+        want='one 1 two'
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+        val='one 000000000001 two'
+        want='one 1 two'
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+    def test_001_suppressleadingzeros_multiple(self):
+        """ Test to suppress one or more leading zeros with multiple occurances"""
+
+        val='one 0001 two 0002 three 3'
+        want='one 1 two 2 three 3'
+        got=test.suppressleadingzeros(val)
+        self.assertEqual(got,want)
+
+    def test_001_spacing(self):
+        """ Test for standard spacing for string with only single spaces """
+
+        val='The quick brown fox jumps over the lazy dog.'
+        want='The quick brown fox jumps over the lazy dog.'
+        got=test.stringspacing(val)
+        self.assertEqual(got,want)
+
+    def test_001_spacing_indent(self):
+        """ Test for standard spacing for string with single spaces and leading indentation """
+
+        val='    The quick brown fox jumps over the lazy dog.'
+        want='The quick brown fox jumps over the lazy dog.'
+        got=test.stringspacing(val)
+        self.assertEqual(got,want)
+
+    def test_001_spacing_multiline(self):
+        """ Test for standard spacing for string with only single spaces """
+
+        val='The quick brown fox\njumps over the lazy dog.'
+        want='The quick brown fox\njumps over the lazy dog.'
+        got=test.stringspacing(val)
+        self.assertEqual(got,want)
+
+    def test_001_spacing_unexpected(self):
+        """ Test for standard spacing for string with unexpected double spaces """
+
+        val='The quick brown  fox jumps over  the lazy dog.'
+        want='The quick brown fox jumps over the lazy dog.'
+        got=test.stringspacing(val)
+        self.assertEqual(got,want)
+
+    def test_001_spacing_unexpected_triple(self):
+        """ Test for standard spacing for string with unexpected triple space """
+
+        val='The quick brown   fox jumps over the lazy dog.'
+        want='The quick brown fox jumps over the lazy dog.'
+        got=test.stringspacing(val)
+        self.assertEqual(got,want)
+
+    def test_001_spacing_singledouble(self):
+        """ Test for standard spacing for string to double spacing """
+
+        val='The quick brown fox jumps over the lazy dog.'
+        want='The  quick  brown  fox  jumps  over  the  lazy  dog.'
+        got=test.stringspacing(val,doublespace=True)
+        self.assertEqual(got,want)
+
+    def test_001_spacing_double(self):
+        """ Test for standard double spacing for string """
+
+        val='The  quick  brown  fox  jumps  over  the  lazy  dog.'
+        want='The  quick  brown  fox  jumps  over  the  lazy  dog.'
+        got=test.stringspacing(val,doublespace=True)
+        self.assertEqual(got,want)
+
+    def test_001_spacing_double_indent(self):
+        """ Test for standard double spacing for string with single spaces and leading indentation """
+
+        val='    The quick brown fox jumps over the lazy dog.'
+        want='The  quick  brown  fox  jumps  over  the  lazy  dog.'
+        got=test.stringspacing(val,doublespace=True)
+        self.assertEqual(got,want)
+
+    def test_001_spacing_double_multiline(self):
+        """ Test for standard spacing for string with only single spaces """
+
+        val='The quick brown fox\njumps over the lazy dog.'
+        want='The  quick  brown  fox\njumps  over  the  lazy  dog.'
+        got=test.stringspacing(val,doublespace=True)
+        self.assertEqual(got,want)
 
 if __name__ == '__main__':
-    main()
+    unittest.main(verbosity=1)
